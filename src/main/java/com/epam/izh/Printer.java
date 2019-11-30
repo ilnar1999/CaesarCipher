4727c1d8a47f2e96cd6c9e7e3c7416fa53f82d4d
Ilnar Khafizov <ilnar.khafizov2019@mail.ru>
2019-11-30 11:20:52 +0400

    Initial commit

---
+++ src/main/java/com/epam/izh/Printer.java
@@ -0,0 +1,11 @@
+package com.epam.izh;
+
+public class Printer {
+    static void printKey(int key) {
+        System.out.println(key);
+    }
+
+    static void printNewText(String newText) {
+        System.out.println(newText + "\n");
+    }
+}
